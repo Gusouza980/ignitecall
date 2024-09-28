c13f7ef96df0ad76817de9a365d99285dead6f3a
Luis Gustavo <gusouza980@gmail.com>
2024-09-28 16:38:28 -0300

    Update

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "@ignite-ui/react";
+
+export default function Home() {
+  return (
+    <>
+      <Heading as="h1">
+        Welcome to Ignite UI for Web Components!
+      </Heading>
+    </>
+  );
+}
